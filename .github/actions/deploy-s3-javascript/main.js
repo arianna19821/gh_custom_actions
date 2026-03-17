c14edede0d05d889cce3d6cab5d42d792c60d28e
arianna19821 <106633871+arianna19821@users.noreply.github.com>
2026-03-17 13:01:33 -0600

    Update main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,1 +1,10 @@
+const core= require ('@actions/core');
+const github= require ('@actions/github');
+const exec= require ('@actions/exec');
 
+
+
+function run(){
+  core.notice ("hola acciones personalizadas via javascrip");
+}
+run();
